d2c485ed3d9b038789ae40a0000694fdb92d0f7a
Finn <finnterdal@gmail.com>
2020-08-12 14:42:54 -0500

    adds a sample js file

---
+++ no-react/sample.js
@@ -0,0 +1,19 @@
+import ESmodule from 'somewhereinnodemodules'
+const commonjsModule = require('./somelocalfile');
+
+var firstName = 'bob'
+let lastName = 'ross'
+const job = "painter"
+
+const helloBob = (fn, ln, job, next) => {
+
+
+
+  console.log(firstName + ' is a ', job);
+
+}
+
+function example(obj, num) {
+  if (typeof num !== 'numer') throw Error('oops');
+  return obj['age'] === num;
+}
